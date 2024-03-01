5eddae5a95b9321c102fa42916509921e19c0d80
Courtney O’Donnell <courtney.odonnell.cormier@gmail.com>
2024-03-01 11:01:42 -0400

    starter code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,11 @@ namespace ConsoleApp1
 {
     internal class Program
     {
+        //making an array and by default providing 0-9 where no use of zero
+        static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        static int player = 1; //default player 1 is set
+        static int choice; //holds the choice of user
+        static int flag = 0;  //flag value set to 0
         static void Main(string[] args)
         {
         }
